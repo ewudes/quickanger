ec67d3c9b5e201d730b13b600b70c6b7e7cc44b7
ewudes <ewudes@gmail.com>
2022-06-25 18:49:16 +0300

    minor fix

--- src/components/authForm/signUp/signUp.js
+++ src/components/authForm/signUp/signUp.js
@@ -6,8 +6,8 @@ import '../authForm.scss'
 export default () => signUp({
   email: input('email', 'Почта', 'email', 'pochta@yandex.ru', 'required', 'authForm__label', 'authForm__field'),
   login: input('login', 'Логин', 'text', 'ivanivanov', 'required', 'authForm__label', 'authForm__field'),
-  firstName: input('firstName', 'Имя', 'text', 'Иван', 'required', 'authForm__label', 'authForm__field'),
-  lastName: input('lastName', 'Фамилия', 'text', 'Иванов', 'required', 'authForm__label', 'authForm__field'),
+  firstName: input('first_name', 'Имя', 'text', 'Иван', 'required', 'authForm__label', 'authForm__field'),
+  lastName: input('second_name', 'Фамилия', 'text', 'Иванов', 'required', 'authForm__label', 'authForm__field'),
   phone: input('phone', 'Телефон', 'phone', '+7 (909) 967 30 30', 'required', 'authForm__label', 'authForm__field'),
   password: input('password', 'Пароль', 'password', '*********', 'required', 'authForm__label', 'authForm__field'),
   repeatPassword: input('password', 'Пароль (ещё раз)', 'password', '*********', 'required', 'authForm__label', 'authForm__field'),
