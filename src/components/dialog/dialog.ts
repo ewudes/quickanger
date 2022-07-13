9c085e9a522d364229c7b7999167f09b31343de7
ewudes <ewudes@gmail.com>
2022-07-13 19:06:39 +0300

    add: dialog layout

--- src/components/dialog/dialog.ts
+++ src/components/dialog/dialog.ts
@@ -1,4 +1,11 @@
 import dialog from './dialog.hbs';
+import input from '../input/input';
+import button from '../button/button';
+import message from './message/message';
 import './dialog.scss'
 
-export default () => dialog()
+export default () => dialog({
+  messageInput: input('message', '', 'text', 'Сообщение', 'required', 'dialog__label', 'dialog__field'),
+  submitBtn: button('Отправить', 'submit', 'submit', 'dialog__submit'),
+  message: message(),
+})
