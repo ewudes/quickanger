6bf0edcb6a50967454c6dcf23753f17998053e73
ewudes <ewudes@gmail.com>
2023-06-22 15:02:24 +0300

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
-import render from "./utils/render";
+// import render from "./utils/render";
 
 import Home from './pages/home/home';
 import Profile from './pages/profile/profile';
 import Auth from './pages/auth/auth';
-import Error from './pages/error/error';
+// import Error from './pages/error/error';
 import './index.scss';
+import router from "./utils/router";
 
-const currentPath: string = window.location.pathname;
+// const currentPath: string = window.location.pathname;
 
-switch(currentPath) {
-  case '/auth/login':
-  case '/auth/registration':
-    render("#app", new Auth());
-    break;
-  case '/profile':
-    render("#app", new Profile());
-    break;
-  case '/':
-    render("#app", new Home());
-    break;
-  default:
-    render("#app", new Error());
-}
+router
+  .use("/", Auth)
+  .use("/registration", Auth)
+  .use("/messenger", Home)
+  .use("/profile", Profile)
+  .start();
+
+
+// switch(currentPath) {
+//   case '/auth/login':
+//   case '/auth/registration':
+//     render("#app", new Auth());
+//     break;
+//   case '/profile':
+//     render("#app", new Profile());
+//     break;
+//   case '/':
+//     render("#app", new Home());
+//     break;
+//   default:
+//     render("#app", new Error());
+// }
